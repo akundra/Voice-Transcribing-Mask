5ef60b568239450b98c04b086df8357a0b26c5c1
akundra <akundra@ucsc.edu>
2021-01-28 23:43:54 -0800

    Added in necessary libraries and tested import

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 # This is a sample Python script.
-
+import speech_recognition as sr
+import pocketsphinx as ps
+import pyaudio as pa
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
 def main():
     # Use a breakpoint in the code line below to debug your script.
-    print("Hello, World")
+    print(sr.__version__)
+    print(pa.get_portaudio_version())
 
 
 # Press the green button in the gutter to run the script.
