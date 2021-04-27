26d8be7b6897bd4608ab03ed6c927dc5dc9c9d7b
akundra <45521198+akundra@users.noreply.github.com>
2021-04-26 22:54:21 -0700

    Updated getBuffer()
    getBuffer() now reads from the command line instead of an array and removed instances of file opening.

--- ScrollArr.py
+++ ScrollArr.py
@@ -1,3 +1,4 @@
+import sys
 from luma.core.interface.serial import spi
 from luma.core.interface.parallel import bitbang_6800
 from luma.core.render import canvas
@@ -10,36 +11,33 @@ from io import BytesIO
 serial = spi(device=0, port=0)
 device = ssd1322(serial)
 
+
 # getBuffer()
 #
-# description: This function takes the string array from the deepspeech voice recognition 
-#              software. Buffer indexing will be handled in external code. If no more
-#              buffers left in array, indicate with a null terminator. 
+# description: This function takes the stdin from the command line and stores it in an array.
 #
-# arguments: buffArr (string array): An array of strings written to and updated by the  
+#
+# arguments: buffArr (string array): An array of strings written to and updated by the
 #                                    voice recognition software.
 #            i (int): The string postion within the imported buffer array buffArr. The
-#                     NewData flag & end of previous buffer triggers i += 1 externally. 
+#                     NewData flag & end of previous buffer triggers i += 1 externally.
 #
 # returns: The new buffer of type string
 
-def getBuffer(buffArr, i):
-    
-    try:
-        buffer = buffArr[i]
-    except:
-        buffer = "\n" # set buffer to
-    
+def getBuffer():
+    buffer = input()
+
     return buffer
+
 
 # getFont()
 #
-# description: This function reads any trutype font seeded in the same directory 
+# description: This function reads any trutype font seeded in the same directory
 #              as the program and outhputs an ImageFont object which can be used
 #              in drawng to the screen. Font size is also adjustale. Default
 #              argument values are currently set.
 #
-# arguments: fontType: A truetype font file (.ttf) to be used with luma.oled 
+# arguments: fontType: A truetype font file (.ttf) to be used with luma.oled
 #                      library draw.text() method.
 #            fontSize : This parameter contorls the physcial size of text on the
 #                      screen. Should range from 20-40. The smaller, the harder
@@ -49,18 +47,18 @@ def getBuffer(buffArr, i):
 # returns:   font: An ImageFont object.
 
 def getFont(fontType="News Gothic MT Regular.ttf", fontSize=35):
-    
     file = open(fontType, "rb")
     bytes_font = BytesIO(file.read())
     font = ImageFont.truetype(bytes_font, fontSize)
     file.close()
-    
+
     return font
+
 
 # getString()
 #
 # description: This function takes the buffer and chunks it into a string of set
-#              length. An input parameter for start postion in buffer will be 
+#              length. An input parameter for start postion in buffer will be
 #              passed in and handled from external code.
 #
 # arguments:   i (int): The postion in the current buffer. Kept track of externally.
@@ -72,98 +70,97 @@ def getFont(fontType="News Gothic MT Regular.ttf", fontSize=35):
 # returns:     A string of length chunklen to be used in text scrolling
 
 def getString(i, buffer, chunklen):
-    
     try:
         string = buffer[i]
     except:
         print("Buffer ends1")
         return ""
 
-    j = i + chunklen          # to consolidate, j could be called chunklen
-    
-    while i < j-1:  # add chunklen to range
+    j = i + chunklen  # to consolidate, j could be called chunklen
+
+    while i < j - 1:  # add chunklen to range
         try:
-            string += buffer[i+1]
+            string += buffer[i + 1]
         except:
             print("Buffer ends2")
-            return string # buffer ended, return the string
+            return string  # buffer ended, return the string
         i += 1
-    
+
     return string
 
+
 # textScroll()
-# 
+#
 # description: This function is called once in main() to scroll the text. It is
-#              the culminating function that ties together the others into a 
-#              cohesive flow. 
+#              the culminating function that ties together the others into a
+#              cohesive flow.
 #
 # arguments: --
 #
-# returns: -- 
+# returns: --
 
 def textScroll():
-    
     x, y = 264, 15
-        
-    sleepMode = False     # this is the flag that sets screen to optimize power consumption
-                      # sleep mode called when string1 < chunklength && x == -string1len (from ImageDraw module)
+
+    sleepMode = False  # this is the flag that sets screen to optimize power consumption
+    # sleep mode called when string1 < chunklength && x == -string1len (from ImageDraw module)
     font = getFont()
-        
-    chunklen = 18   # sets length of text chunking (can experiment with smoothness: the smaller the faster)
-    chunks = 100     # sets number of max chunks
-    maxbuff = chunks*chunklen   # use later for testing if last buffer
-    
-    file = open("words.txt", "r")  # open file ONCE before while loop
-    buffer = getBuffer(file, chunklen, chunks)
-    
-    sloop = 0            # string looper to obtain strings from buffer
-    newBuf = False       # only matters at second new buff
-       
+
+    chunklen = 18  # sets length of text chunking (can experiment with smoothness: the smaller the faster)
+    chunks = 100  # sets number of max chunks
+    # maxbuff = chunks * chunklen  # use later for testing if last buffer
+
+    # file = open("words.txt", "r")  # open file ONCE before while loop
+    buffer = getBuffer()
+
+    sloop = 0  # string looper to obtain strings from buffer
+    newBuf = False  # only matters at second new buff
+
     string1 = getString(sloop, buffer, chunklen)  # get first string from buffer
-    sloop += len(string1) # could be chunklen + 1
-    
-    string2 = getString(sloop, buffer, chunklen)   # get second string from buffer
-    sloop +=len(string2) 
-     
-    string = string1 + string2    # concatanate strings for smooth scrolling
-    
+    sloop += len(string1)  # could be chunklen + 1
+
+    string2 = getString(sloop, buffer, chunklen)  # get second string from buffer
+    sloop += len(string2)
+
+    string = string1 + string2  # concatenate strings for smooth scrolling
+
     print(string)
-    
+
     print(string2)
-    
-    while sleepMode == False:
-        
+
+    while not sleepMode:
+
         with canvas(device) as draw:
-            
-            draw.text((x,y), string, fill=None, font=font)
-        
+
+            draw.text((x, y), string, fill=None, font=font)
+
         s1len, s1high = draw.multiline_textsize(string1, font=font)
-             
+
         x -= 4
-            
+
         if x <= -s1len:
-            
+
             # Check if when s1 scrolls off if there is no more incoing string data
             if string2 == "\n":
-                deltaBuf = False # to calculate a change in buffer
-                ts = time.perf_counter() # set present time
-                
+                deltaBuf = False  # to calculate a change in buffer
+                ts = time.perf_counter()  # set present time
+
                 # Try to get new form of buffer or new buffer until file is changed, then use buffer
-                while deltaBuf == False:
+                while not deltaBuf:
                     prevbuffer = buffer
-                    newbuffer = getBuffer(file, chunklen, chunks)
-                    tp = time.perf_counter() - ts # difference between present time and start of no text
+                    newbuffer = getBuffer()
+                    tp = time.perf_counter() - ts  # difference between present time and start of no text
                     # print(tp)
-                    #if tp >= 10:
-                        # clear buffer
-                        # search new
-                        # if not found in 1 minute, exit program?
-                        # get new buffer
-                        # signal jump out
-                    
+                    # if tp >= 10:
+                    # clear buffer
+                    # search new
+                    # if not found in 1 minute, exit program?
+                    # get new buffer
+                    # signal jump out
+
                     if newbuffer != "":
                         deltaBuf = True
-                        buffer = prevbuffer + newbuffer # append new words to previous buffer for new buffer
+                        buffer = prevbuffer + newbuffer  # append new words to previous buffer for new buffer
                         print(sloop)
                         print(prevbuffer)
                         string1 = getString(sloop, buffer, chunklen)
@@ -174,9 +171,9 @@ def textScroll():
                         print(string2)
                         newBuf = True
                 print("reached")
-            
+
             if len(string1) < chunklen:
-                if newBuf == True:
+                if newBuf:
                     newBuf = False
                     x = 264
                 else:
@@ -185,7 +182,7 @@ def textScroll():
                 string2 = "\n"
                 string = string1  # dont calculate OR add another string cuz end.
             else:
-                if newBuf == True:  # Let's bypass normal operations coming out of finding new buffer. 
+                if newBuf:  # Let's bypass normal operations coming out of finding new buffer.
                     newBuf = False
                     string = string1 + string2
                     x = 264
@@ -193,24 +190,24 @@ def textScroll():
                     string1 = string2
                     string2 = getString(sloop, buffer, chunklen)
                     sloop += len(string2)
-                    string = string1 + string2 
-                    x = 0                                    # Reset
-                
+                    string = string1 + string2
+                    x = 0  # Reset
+
             print(string)
             print(len(string1))
             print(len(buffer))
-    
-    file.close()
-        
+
+    # file.close()
+
     return
 
 
-def main():  
-
+def main():
+    for line in sys.stdin:
+        if 'start_transcribe' == line.rstrip():
+            break
     textScroll()
 
- 
+
 if __name__ == "__main__":
     main()
-
-
